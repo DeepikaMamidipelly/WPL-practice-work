42070529a78647eaa1a6f6e54c2de9476b350314
DeepikaMamidipelly <deepikarajmamidipelly@gmail.com>
2022-04-01 20:20:13 -0500

    https://github.com/oluwatoyinn/academia.git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, {Component} from "react";
 
+
+
+
+class App extends Component{
+
+  state ={
+    videos: []
+  }
+
+  componentDidMount(){
+    fetch('videos.json',{
+      headers:{
+        'Content-Type':'application/json',
+        'Accept':'application/json'
+      }
+    }).then(res => res.json())
+  }
+  render(){
+    return(
+       <div>
+          <h1>Video List</h1>
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Hamilton</h5>
+              <h6 className="card-subtitle mb-2 text-muted">Musical Drama</h6>
+              <p className="card-text">An American Musical is a sung-and-rapped-through musical by Lin-Manuel Miranda. It tells the story of American Founding Father Alexander Hamilton. Miranda said he was inspired to write the musical after reading the 2004 biography Alexander Hamilton by Ron Chernow.</p>
+            </div>
+          </div>
+        </div>
+    );
+  }
+}
 export default App;
